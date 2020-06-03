1fb13a929096359f0ce7aabec087f05750f6df35
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2020-06-03 19:27:51 +0200

    Added namespace for the plugin scripts

--- Assets/CustomScriptTemplate/CustomScriptTemplate.cs
+++ Assets/CustomScriptTemplate/CustomScriptTemplate.cs
@@ -1,139 +1,139 @@
 ﻿#if UNITY_EDITOR
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using UnityEditor;
 using UnityEngine;
 
-public class CustomScriptTemplate
+namespace CustomScriptTemplate
 {
-    public const string TEMPLATES_DIRECTORY_KEY = "templatesDirectory";
-    public const string DEFAULT_TEMPLATES_DIRECTORY = "ScriptTemplates";
-
-    public const string NAMESPACE_ROOT_DIR_KEY = "namespaceRootDir";
-    public const string DEFAULT_NAMESPACE_ROOT_DIR = "Scripts";
-
-    public const string NAMESPACE_PREFIX_KEY = "namespacePrefix";
-    public const string DEFAULT_NAMESSPACE_PREFIX = "Company.Product";
-
-    public const string CLASS_NAME_WILDCARD = "@{CLASS_NAME}@";
-    public const string NAMESPACE_WILDCARD = "@{NAMESPACE}@";
-    public const string NAMESPACE_PREFIX_WILDCARD = "@{NAMESPACE_PREFIX}@";
-
-    public static void CreateTemplate(string templateName)
+    public class CustomScriptTemplate
     {
-        // create the template file
-        File.WriteAllText(GetTemplatePath(templateName), GetDefaultTemplateContent());
+        public const string TEMPLATES_DIRECTORY_KEY = "templatesDirectory";
+        public const string DEFAULT_TEMPLATES_DIRECTORY = "ScriptTemplates";
 
-        // create the menu script
-        var menuScriptContent = GetMenuScriptTemplateContent();
-        var className = templateName.Replace(".txt", "");
-        menuScriptContent = menuScriptContent.Replace(CLASS_NAME_WILDCARD, className);
-        File.WriteAllText(GetMenuScriptPath(templateName), menuScriptContent);
-    }
+        public const string NAMESPACE_ROOT_DIR_KEY = "namespaceRootDir";
+        public const string DEFAULT_NAMESPACE_ROOT_DIR = "Scripts";
 
-    public static void CreateScriptFromTemplate(string scriptPath, string className, string templateName)
-    {
-        className = className.Trim();
-        if (className.EndsWith(".cs"))
-            className = className.Substring(0, className.Length - 3);
+        public const string NAMESPACE_PREFIX_KEY = "namespacePrefix";
+        public const string DEFAULT_NAMESSPACE_PREFIX = "Company.Product";
 
-        var namespacePrefix = GetNamespacePrefix();
-        var namespaceForPath = GetNamespaceForPath(scriptPath);
-        if (!string.IsNullOrEmpty(namespacePrefix) && !string.IsNullOrEmpty(namespaceForPath))
-            namespacePrefix += '.';
+        public const string CLASS_NAME_WILDCARD = "@{CLASS_NAME}@";
+        public const string NAMESPACE_WILDCARD = "@{NAMESPACE}@";
+        public const string NAMESPACE_PREFIX_WILDCARD = "@{NAMESPACE_PREFIX}@";
 
-        var templateContent = File.ReadAllText(GetTemplatePath(templateName));
-        templateContent = templateContent.Replace(CLASS_NAME_WILDCARD, className);
-        templateContent = templateContent.Replace(NAMESPACE_WILDCARD, namespaceForPath);
-        templateContent = templateContent.Replace(NAMESPACE_PREFIX_WILDCARD, namespacePrefix);
-
-        File.WriteAllText(Path.Combine(scriptPath, $"{className}.cs"), templateContent);
-    }
-
-    public static void EditTemplate(string templateName, string content)
-    {
-        File.WriteAllText(GetTemplatePath(templateName), content);
-    }
-
-    public static void DeleteTemplate(string templateName)
-    {
-        File.Delete(GetTemplatePath(templateName));
-        File.Delete(GetMenuScriptPath(templateName));
-    }
-
-    public static string GetTemplatesDirectory()
-    {
-        return EditorPrefs.GetString(TEMPLATES_DIRECTORY_KEY, DEFAULT_TEMPLATES_DIRECTORY);
-    }
-
-    public static void SetTemplatesDirectory(string directory)
-    {
-        EditorPrefs.SetString(TEMPLATES_DIRECTORY_KEY, directory);
-    }
-
-    public static string GetNamespaceRootDir()
-    {
-        return EditorPrefs.GetString(NAMESPACE_ROOT_DIR_KEY, DEFAULT_NAMESPACE_ROOT_DIR);
-    }
-
-    public static void SetNamespaceRootDir(string rootDir)
-    {
-        EditorPrefs.SetString(NAMESPACE_ROOT_DIR_KEY, rootDir);
-    }
-
-    public static string GetNamespacePrefix()
-    {
-        return EditorPrefs.GetString(NAMESPACE_PREFIX_KEY, DEFAULT_NAMESSPACE_PREFIX);
-    }
-
-    public static void SetNamespacePrefix(string prefix)
-    {
-        EditorPrefs.SetString(NAMESPACE_PREFIX_KEY, prefix);
-    }
-
-    public static string GetTemplatePath(string templateName)
-    {
-        return $"Assets/{GetTemplatesDirectory()}/{templateName}.txt";
-    }
-
-    public static string GetMenuScriptPath(string templateName)
-    {
-        return $"Assets/{GetTemplatesDirectory()}/{templateName}_MenuScript.cs";
-    }
-
-    private static string GetDefaultTemplateContent()
-    {
-        var defaultScriptTemplateAsset = Resources.Load<TextAsset>("DefaultScriptTemplate");
-        return defaultScriptTemplateAsset.text;
-    }
-
-    private static string GetMenuScriptTemplateContent()
-    {
-        var menuScriptTemplateAsset = Resources.Load<TextAsset>("MenuScriptTemplate");
-        return menuScriptTemplateAsset.text;
-    }
-
-    private static string GetNamespaceForPath(string path)
-    {
-        if (string.IsNullOrEmpty(path))
-            return string.Empty;
-
-        var rootDir = Path.Combine("Assets", GetNamespaceRootDir());
-        if (string.IsNullOrEmpty(rootDir))
-            return path.Trim('\\', '/', '.').Replace('\\', '.').Replace('/', '.');
-
-        var pathAbs = Path.GetFullPath(path);
-        var rootDirAbs = Path.GetFullPath(rootDir);
-        Debug.Log(pathAbs);
-        Debug.Log(rootDirAbs);
-        if(pathAbs.StartsWith(rootDirAbs))
+        public static void CreateTemplate(string templateName)
         {
-            path = pathAbs.Substring(rootDirAbs.Length);
+            // create the template file
+            File.WriteAllText(GetTemplatePath(templateName), GetDefaultTemplateContent());
+
+            // create the menu script
+            var menuScriptContent = GetMenuScriptTemplateContent();
+            var className = templateName.Replace(".txt", "");
+            menuScriptContent = menuScriptContent.Replace(CLASS_NAME_WILDCARD, className);
+            File.WriteAllText(GetMenuScriptPath(templateName), menuScriptContent);
         }
 
-        return path.Trim('\\', '/', '.').Replace('\\', '.').Replace('/', '.');
+        public static void CreateScriptFromTemplate(string scriptPath, string className, string templateName)
+        {
+            className = className.Trim();
+            if (className.EndsWith(".cs"))
+                className = className.Substring(0, className.Length - 3);
+
+            var namespacePrefix = GetNamespacePrefix();
+            var namespaceForPath = GetNamespaceForPath(scriptPath);
+            if (!string.IsNullOrEmpty(namespacePrefix) && !string.IsNullOrEmpty(namespaceForPath))
+                namespacePrefix += '.';
+
+            var templateContent = File.ReadAllText(GetTemplatePath(templateName));
+            templateContent = templateContent.Replace(CLASS_NAME_WILDCARD, className);
+            templateContent = templateContent.Replace(NAMESPACE_WILDCARD, namespaceForPath);
+            templateContent = templateContent.Replace(NAMESPACE_PREFIX_WILDCARD, namespacePrefix);
+
+            File.WriteAllText(Path.Combine(scriptPath, $"{className}.cs"), templateContent);
+        }
+
+        public static void EditTemplate(string templateName, string content)
+        {
+            File.WriteAllText(GetTemplatePath(templateName), content);
+        }
+
+        public static void DeleteTemplate(string templateName)
+        {
+            File.Delete(GetTemplatePath(templateName));
+            File.Delete(GetMenuScriptPath(templateName));
+        }
+
+        public static string GetTemplatesDirectory()
+        {
+            return EditorPrefs.GetString(TEMPLATES_DIRECTORY_KEY, DEFAULT_TEMPLATES_DIRECTORY);
+        }
+
+        public static void SetTemplatesDirectory(string directory)
+        {
+            EditorPrefs.SetString(TEMPLATES_DIRECTORY_KEY, directory);
+        }
+
+        public static string GetNamespaceRootDir()
+        {
+            return EditorPrefs.GetString(NAMESPACE_ROOT_DIR_KEY, DEFAULT_NAMESPACE_ROOT_DIR);
+        }
+
+        public static void SetNamespaceRootDir(string rootDir)
+        {
+            EditorPrefs.SetString(NAMESPACE_ROOT_DIR_KEY, rootDir);
+        }
+
+        public static string GetNamespacePrefix()
+        {
+            return EditorPrefs.GetString(NAMESPACE_PREFIX_KEY, DEFAULT_NAMESSPACE_PREFIX);
+        }
+
+        public static void SetNamespacePrefix(string prefix)
+        {
+            EditorPrefs.SetString(NAMESPACE_PREFIX_KEY, prefix);
+        }
+
+        public static string GetTemplatePath(string templateName)
+        {
+            return $"Assets/{GetTemplatesDirectory()}/{templateName}.txt";
+        }
+
+        public static string GetMenuScriptPath(string templateName)
+        {
+            return $"Assets/{GetTemplatesDirectory()}/{templateName}_MenuScript.cs";
+        }
+
+        private static string GetDefaultTemplateContent()
+        {
+            var defaultScriptTemplateAsset = Resources.Load<TextAsset>("DefaultScriptTemplate");
+            return defaultScriptTemplateAsset.text;
+        }
+
+        private static string GetMenuScriptTemplateContent()
+        {
+            var menuScriptTemplateAsset = Resources.Load<TextAsset>("MenuScriptTemplate");
+            return menuScriptTemplateAsset.text;
+        }
+
+        private static string GetNamespaceForPath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+
+            var rootDir = Path.Combine("Assets", GetNamespaceRootDir());
+            if (string.IsNullOrEmpty(rootDir))
+                return path.Trim('\\', '/', '.').Replace('\\', '.').Replace('/', '.');
+
+            var pathAbs = Path.GetFullPath(path);
+            var rootDirAbs = Path.GetFullPath(rootDir);
+            Debug.Log(pathAbs);
+            Debug.Log(rootDirAbs);
+            if (pathAbs.StartsWith(rootDirAbs))
+            {
+                path = pathAbs.Substring(rootDirAbs.Length);
+            }
+
+            return path.Trim('\\', '/', '.').Replace('\\', '.').Replace('/', '.');
+        }
     }
 }
 #endif
--- Assets/CustomScriptTemplate/Editor/CustomScriptTemplateEditor.cs
+++ Assets/CustomScriptTemplate/Editor/CustomScriptTemplateEditor.cs
@@ -1,139 +1,140 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEditor;
 using System.IO;
 
-public class CustomScriptTemplateEditor : EditorWindow
+namespace CustomScriptTemplate
 {
-    [MenuItem("Assets/Custom Script Templates")]
-    public static void ShowWindow()
+    public class CustomScriptTemplateEditor : EditorWindow
     {
-        GetWindow<CustomScriptTemplateEditor>();
-    }
-
-    private string _templatesDirectory;
-
-    private int _selectedScriptIndex;
-    private string[] _scripts;
-
-    private int _readScript = -1;
-    private string _editScript;
-
-    private string _createTemplateName = "";
-
-    private string _namespaceRootDirectory = "";
-    private string _namespacePrefix = "";
-
-    private void Awake()
-    {
-        _templatesDirectory = CustomScriptTemplate.GetTemplatesDirectory();
-        _namespaceRootDirectory = CustomScriptTemplate.GetNamespaceRootDir();
-        _namespacePrefix = CustomScriptTemplate.GetNamespacePrefix();
-    }
-
-    void OnGUI()
-    {
-        EditorGUILayout.BeginVertical();
+        [MenuItem("Assets/Custom Script Templates")]
+        public static void ShowWindow()
         {
-            /* === TEMPLATES === */
-
-            EditorGUILayout.Separator();
-            EditorGUILayout.LabelField("Templates", new GUIStyle() { fontSize = 16 });
-
-            FindTemplates();
-            _selectedScriptIndex = EditorGUILayout.Popup(_selectedScriptIndex, _scripts);
-
-            if (_scripts != null && _scripts.Length > 0 && _readScript != _selectedScriptIndex)
-            {
-                var selectedScriptName = _scripts[_selectedScriptIndex];
-                _editScript = File.ReadAllText(GetTemplatePath(selectedScriptName));
-                _readScript = _selectedScriptIndex;
-            }
-            _editScript = EditorGUILayout.TextArea(_editScript);
-
-            if (GUILayout.Button("Open in editor") && _scripts != null && _scripts.Length > 0)
-            {
-                System.Diagnostics.Process.Start($"{Directory.GetCurrentDirectory()}/{GetTemplatePath(_scripts[_selectedScriptIndex])}");
-            }
-
-            if (GUILayout.Button("Save") && _scripts != null && _scripts.Length > 0)
-            {
-                File.WriteAllText(GetTemplatePath(_scripts[_selectedScriptIndex]), _editScript);
-            }
-
-            if (GUILayout.Button("Delete") && _scripts != null && _scripts.Length > 0)
-            {
-                CustomScriptTemplate.DeleteTemplate(_scripts[_selectedScriptIndex]);
-                AssetDatabase.Refresh();
-
-                _selectedScriptIndex = 0;
-                _readScript = -1;
-                _editScript = "";
-            }
-
-            /* === CREATE NEW TEMPLATE === */
-
-            EditorGUILayout.Separator();
-            EditorGUILayout.LabelField("Create new template", new GUIStyle() { fontSize = 16 });
-
-            EditorGUILayout.LabelField("Template name:");
-            _createTemplateName = EditorGUILayout.TextField(_createTemplateName);
-            if(GUILayout.Button("Create"))
-            {
-                CustomScriptTemplate.CreateTemplate(_createTemplateName);
-                AssetDatabase.Refresh();
-            }
-
-            /* === SETTINGS === */
-
-            EditorGUILayout.Separator();
-            EditorGUILayout.LabelField("Settings", new GUIStyle() { fontSize = 16 });
-            EditorGUILayout.LabelField("Templates directory (relative to Assets):");
-            var templatesDirectory = EditorGUILayout.TextField(_templatesDirectory);
-            if (templatesDirectory != _templatesDirectory)
-            {
-                _templatesDirectory = templatesDirectory;
-                CustomScriptTemplate.SetTemplatesDirectory(_templatesDirectory);
-            }
-
-            EditorGUILayout.LabelField("Namespace root directory (relative to Assets):");
-            var namespaceRootDir = EditorGUILayout.TextField(_namespaceRootDirectory);
-            if(namespaceRootDir != _namespaceRootDirectory)
-            {
-                _namespaceRootDirectory = namespaceRootDir;
-                CustomScriptTemplate.SetNamespaceRootDir(_namespaceRootDirectory);
-            }
-
-            EditorGUILayout.LabelField("Namespace prefix:");
-            var namespacePrefix = EditorGUILayout.TextField(_namespacePrefix);
-            if(namespacePrefix != _namespacePrefix)
-            {
-                _namespacePrefix = namespacePrefix;
-                CustomScriptTemplate.SetNamespacePrefix(_namespacePrefix);
-            }
-        }
-        EditorGUILayout.EndVertical();
-    }
-
-    private void FindTemplates()
-    {
-        if(!Directory.Exists($"Assets/{_templatesDirectory}"))
-        {
-            Directory.CreateDirectory($"Assets/{_templatesDirectory}");
+            GetWindow<CustomScriptTemplateEditor>();
         }
 
-        var files = Directory.GetFiles($"Assets/{_templatesDirectory}", "*.txt");
-        _scripts = new string[files.Length];
+        private string _templatesDirectory;
 
-        for(var i = 0; i < files.Length; i++)
+        private int _selectedScriptIndex;
+        private string[] _scripts;
+
+        private int _readScript = -1;
+        private string _editScript;
+
+        private string _createTemplateName = "";
+
+        private string _namespaceRootDirectory = "";
+        private string _namespacePrefix = "";
+
+        private void Awake()
         {
-            _scripts[i] = Path.GetFileNameWithoutExtension(files[i]);
+            _templatesDirectory = CustomScriptTemplate.GetTemplatesDirectory();
+            _namespaceRootDirectory = CustomScriptTemplate.GetNamespaceRootDir();
+            _namespacePrefix = CustomScriptTemplate.GetNamespacePrefix();
         }
-    }
 
-    private string GetTemplatePath(string templateName)
-    {
-        return CustomScriptTemplate.GetTemplatePath(templateName);
+        void OnGUI()
+        {
+            EditorGUILayout.BeginVertical();
+            {
+                /* === TEMPLATES === */
+
+                EditorGUILayout.Separator();
+                EditorGUILayout.LabelField("Templates", new GUIStyle() { fontSize = 16 });
+
+                FindTemplates();
+                _selectedScriptIndex = EditorGUILayout.Popup(_selectedScriptIndex, _scripts);
+
+                if (_scripts != null && _scripts.Length > 0 && _readScript != _selectedScriptIndex)
+                {
+                    var selectedScriptName = _scripts[_selectedScriptIndex];
+                    _editScript = File.ReadAllText(GetTemplatePath(selectedScriptName));
+                    _readScript = _selectedScriptIndex;
+                }
+                _editScript = EditorGUILayout.TextArea(_editScript);
+
+                if (GUILayout.Button("Open in editor") && _scripts != null && _scripts.Length > 0)
+                {
+                    System.Diagnostics.Process.Start($"{Directory.GetCurrentDirectory()}/{GetTemplatePath(_scripts[_selectedScriptIndex])}");
+                }
+
+                if (GUILayout.Button("Save") && _scripts != null && _scripts.Length > 0)
+                {
+                    File.WriteAllText(GetTemplatePath(_scripts[_selectedScriptIndex]), _editScript);
+                }
+
+                if (GUILayout.Button("Delete") && _scripts != null && _scripts.Length > 0)
+                {
+                    CustomScriptTemplate.DeleteTemplate(_scripts[_selectedScriptIndex]);
+                    AssetDatabase.Refresh();
+
+                    _selectedScriptIndex = 0;
+                    _readScript = -1;
+                    _editScript = "";
+                }
+
+                /* === CREATE NEW TEMPLATE === */
+
+                EditorGUILayout.Separator();
+                EditorGUILayout.LabelField("Create new template", new GUIStyle() { fontSize = 16 });
+
+                EditorGUILayout.LabelField("Template name:");
+                _createTemplateName = EditorGUILayout.TextField(_createTemplateName);
+                if (GUILayout.Button("Create"))
+                {
+                    CustomScriptTemplate.CreateTemplate(_createTemplateName);
+                    AssetDatabase.Refresh();
+                }
+
+                /* === SETTINGS === */
+
+                EditorGUILayout.Separator();
+                EditorGUILayout.LabelField("Settings", new GUIStyle() { fontSize = 16 });
+                EditorGUILayout.LabelField("Templates directory (relative to Assets):");
+                var templatesDirectory = EditorGUILayout.TextField(_templatesDirectory);
+                if (templatesDirectory != _templatesDirectory)
+                {
+                    _templatesDirectory = templatesDirectory;
+                    CustomScriptTemplate.SetTemplatesDirectory(_templatesDirectory);
+                }
+
+                EditorGUILayout.LabelField("Namespace root directory (relative to Assets):");
+                var namespaceRootDir = EditorGUILayout.TextField(_namespaceRootDirectory);
+                if (namespaceRootDir != _namespaceRootDirectory)
+                {
+                    _namespaceRootDirectory = namespaceRootDir;
+                    CustomScriptTemplate.SetNamespaceRootDir(_namespaceRootDirectory);
+                }
+
+                EditorGUILayout.LabelField("Namespace prefix:");
+                var namespacePrefix = EditorGUILayout.TextField(_namespacePrefix);
+                if (namespacePrefix != _namespacePrefix)
+                {
+                    _namespacePrefix = namespacePrefix;
+                    CustomScriptTemplate.SetNamespacePrefix(_namespacePrefix);
+                }
+            }
+            EditorGUILayout.EndVertical();
+        }
+
+        private void FindTemplates()
+        {
+            if (!Directory.Exists($"Assets/{_templatesDirectory}"))
+            {
+                Directory.CreateDirectory($"Assets/{_templatesDirectory}");
+            }
+
+            var files = Directory.GetFiles($"Assets/{_templatesDirectory}", "*.txt");
+            _scripts = new string[files.Length];
+
+            for (var i = 0; i < files.Length; i++)
+            {
+                _scripts[i] = Path.GetFileNameWithoutExtension(files[i]);
+            }
+        }
+
+        private string GetTemplatePath(string templateName)
+        {
+            return CustomScriptTemplate.GetTemplatePath(templateName);
+        }
     }
 }
